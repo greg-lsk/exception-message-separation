339f714c443b50d24b5ecf5b933db7d584b800e8
Greg Liaskas <greg.liaskas@outlook.com>
2025-11-21 14:12:54 +0200

    docs:: XML documentatin comments

--- ExceptionMessageSeparation/IExceptionMessage.cs
+++ ExceptionMessageSeparation/IExceptionMessage.cs
@@ -1,6 +1,15 @@
 ﻿namespace ExceptionMessageSeparation;
 
+/// <summary>
+/// Defines a contract for creating exception messages.
+/// </summary>
+/// <typeparam name="TException">The type of the exception that this interface applies to.</typeparam>
 public interface IExceptionMessage<TException> where TException : Exception
 {
+    /// <summary>
+    /// Creates the message for the specified exception.
+    /// </summary>
+    /// <param name="exception">The exception for which to generate the message.</param>
+    /// <returns>A string representing the message for the exception.</returns>
     public string For(TException exception);
 }
